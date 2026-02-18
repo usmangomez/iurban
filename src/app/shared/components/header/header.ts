b5483739795e7fa754252c8fb5cafb67a818f4f7
usman <usmangomezabeuov@gmail.com>
2026-02-18 22:43:45 +0100

    Remove unused TranslatePipe from header component and add pointer cursor to card hover effect.

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,10 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TranslatePipe } from '../../pipes/translate.pipe';
 import { LanguageSwitcher } from '../language-switcher/language-switcher';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, TranslatePipe, LanguageSwitcher],
+  imports: [RouterLink, LanguageSwitcher],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
